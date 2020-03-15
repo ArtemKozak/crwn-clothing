9feaca9c452c6e5cf9f92ed19a756beee561d01c
Druid72 <timon4k37@gmail.com>
2020-03-15 22:12:12 +0200

    finish styling menu item

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomePage from "./pages/hompage/hompage.component";
+import HomePage from "./pages/hompage/homepage.component";
 import './App.css';
 
 function App() {
