ff63e13503127fe058e0cf1ac5541b703f561090
Druid72 <timon4k37@gmail.com>
2020-03-15 19:37:55 +0200

    adding homepage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import HomePage from "./components/hompage/hompage.component";
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello world!
+      <HomePage />
     </div>
   );
 }
