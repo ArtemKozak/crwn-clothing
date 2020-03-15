ab7063066f18045cb685ef8d48e13a5d931599a9
Druid72 <timon4k37@gmail.com>
2020-03-16 00:35:22 +0200

    adding collection preview

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <div>
             <Switch>
                 <Route exact path='/' component={HomePage} />
-                <Route path='/shop' component={ShopPage} />
+                <Route exact path='/shop' component={ShopPage} />
             </Switch>
         </div>
     );
